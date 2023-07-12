aafde717d88a8f672ed5c5b6acc43d6e45c61797
tienpbss <tienphung.cn07@gmail.com>
2023-07-12 10:06:10 +0700

    delete mycode commented in controllers.role, edit testRole

--- src/controllers/role.controllers.js
+++ src/controllers/role.controllers.js
@@ -1,72 +1,3 @@
-// const Role = require('../models/role.models')
-
-// module.exports.createRole = async (req, res) => {
-//     const { name, permissions } = req.body;
-//     const newRole = new Role({
-//         name,
-//         permissions
-//     })
-//     try{
-//         const result = await newRole.save();
-//         res.status(200).json({ result });        
-//     }
-//     catch(err) {
-//         console.log(err);
-//         res.status(500).json({ err: "error when create new role"});
-//     }
-
-// }
-
-// module.exports.viewAllRoles = async (req, res) => {
-//     try{
-//         const roles = await Role.find();
-//         res.status(200).json({ roles });        
-//     }
-//     catch(err){
-//         console.log(err);
-//         res.status()
-//     }
-
-// }
-
-// module.exports.viewRole = async (req, res) => {
-//     const { _idRole }  = req.params
-//     try{
-//         const role = await Role.findById(_idRole);
-//         res.status(200).json({ role });        
-//     }
-//     catch(err){
-//         console.log(err);
-//         res.status(404).json({ err: 'error when find role'});
-//     }
-// }
-
-// module.exports.editRole = async (req, res) => {
-//     const { _idRole } = req.params;
-//     const { name, permissions } = req.body;
-//     try {
-//         const updatedRole = await Role.findByIdAndUpdate(_idRole, { name, permissions }, { new: true});
-//         res.status(200).json({ updatedRole });
-//     }
-//     catch(err){
-//         console.log(err);
-//         res.status(500).json({ err: 'error when update role'});
-//     }
-// }
-
-// module.exports.deleteRole = async (req, res) => {
-//     const { _idRole } = req.params;
-//     try {
-//         const removedRole = await Role.findByIdAndDelete(_idRole);
-//         res.status(200).json({ removedRole })
-//     }
-//     catch(err){
-//         console.log(err);
-//         res.status(500).json({ err: 'error when delete role'});
-//     }
-// }
-
-
 const Role = require('../models/role.models');
 
 exports.createRole = async (req, res) => {
@@ -142,7 +73,6 @@ exports.deleteRole = async (req, res) => {
   try {
     const { _idRole } = req.params;
     const removedRole = await Role.findByIdAndDelete(_idRole);
-    console.log(removedRole);
     if (!removedRole) {
       return res.status(404).json({ error: 'Role not found' });
     }
